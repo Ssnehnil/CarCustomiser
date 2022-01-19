bb0eca77f58a19640d98d82f0aee934c3a8e173b
Ssnehnil <ghosh.s@etoncollege.org.uk>
2022-01-19 20:41:06 +0000

    Initial Commit

---
+++ Car Customiser/Car_CustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_CustomiserApp.swift
+//  Car Customiser
+//
+//  Created by Ssnehnil Ghosh on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Car_CustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
